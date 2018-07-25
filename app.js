8dcd013f786d10b1c232fd152e657c4ecde4d09d
unknown <wakaspar@gmail.com>
2018-07-25 12:13:05 -0700

    adds bg music

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 console.log('hello world!');
 
 $(document).ready(function(){
-  $('.page-header').css('color', 'red');
 
+
+
+  // var $bgmusic = $('#bg-music')[0];
+  // $bgmusic.play();
+  // $('bg-music').get(0).play();
+
+  $('#music-button').on('click', function(){
+    $('#bg-music')[0].play();
+  })
+
+  $('.page-header').css('color', 'red');
   $('img').on('click', function(){
     $('.page-header').css('color', 'rgb(104, 214, 154)');
   })
-
 
 }) // close document.ready
